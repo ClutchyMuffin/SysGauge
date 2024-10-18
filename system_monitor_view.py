8a3d2ec0b59af2b2e6ddea8077cdc46db7e9b2d0
Farzaan <syedfarzaanrafi@gmail.com>
2024-10-17 23:05:44 -0600

    Implemented the functions to create & setup the different layouts

--- system_monitor_view.py
+++ system_monitor_view.py
@@ -1,5 +1,7 @@
 from system_metrics import SystemMetrics
-from PyQt5.QtWidgets import QMainWindow, QWidget, QVBoxLayout
+from PyQt5.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QLabel
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 
 # The SystemMonitorView class is a subclass of QMainWindow
 class SystemMonitorView(QMainWindow):
@@ -29,13 +31,41 @@ class SystemMonitorView(QMainWindow):
 
     # Create the metrics layout (top bar)
     def create_metrics_layout(self):
-        print("Creating metrics layout")
-        # add stuff here
+        self.metrics_layout = QHBoxLayout()
+        self.main_layout.addLayout(self.metrics_layout)
+
+        # Labels
+        self.cpu_label = QLabel("CPU Usage: ")
+        self.mem_label = QLabel("Memory Usage: ")
+        self.disk_label = QLabel("Disk Usage: ")
+        self.net_label = QLabel("Network (sent/received): ")
+
+        self.metrics_layout.addWidget(self.cpu_label)
+        self.metrics_layout.addWidget(self.mem_label)
+        self.metrics_layout.addWidget(self.disk_label)
+        self.metrics_layout.addWidget(self.net_label)
+
 
     # Create the plot layout
     def create_plot_layout(self):
-        print("Creating plot layout")
-        # add stuff here
+        self.plot_layout = QVBoxLayout()
+        self.main_layout.addLayout(self.plot_layout)
+
+        # CPU Usage Graph
+        self.cpu_fig = Figure()
+        self.cpu_ax = self.cpu_fig.add_subplot(111)
+        self.cpu_ax.set_title('CPU Usage')
+        self.cpu_ax.set_ylim(0, 100)
+        self.cpu_canvas = FigureCanvas(self.cpu_fig)
+        self.plot_layout.addWidget(self.cpu_canvas)
+
+        # Memory Usage Graph
+        self.mem_fig = Figure()
+        self.mem_ax = self.mem_fig.add_subplot(111)
+        self.mem_ax.set_title('Memory Usage')
+        self.mem_ax.set_ylim(0, 100)
+        self.mem_canvas = FigureCanvas(self.mem_fig)
+        self.plot_layout.addWidget(self.mem_canvas)
 
     # Update the metrics displayed in the labels
     def update_metrics(self):
